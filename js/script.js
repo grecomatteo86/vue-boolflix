88fadf8e23ddb7feb0462b19a44207a15f9bd789
Matteo <grecomatteo86@gmail.com>
2021-04-09 17:22:09 +0200

    Performing a GET static request

--- js/script.js
+++ js/script.js
@@ -8,9 +8,22 @@ var app = new Vue (
     },
     methods:{
       searchTitle: function(){
-        // request to the server
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
+        // static request to the server
+        // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
+        // .then((response) => {
+        //   this.movies = response.data.results;
+        // });
+
+        // static request to the server
+        axios.get('https://api.themoviedb.org/3/search/movie',{
+          params:{
+            api_key:"bb946051787722f6361023f25c0639b5",
+            query:"ritorno al fut",
+            language:"it-IT"
+          }
+        })
         .then((response) => {
+          // console.log(response);
           this.movies = response.data.results;
         });
       }
