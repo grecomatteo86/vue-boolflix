84b4511f1aae302e2a8b89f55e918223352fdea7
Matteo <grecomatteo86@gmail.com>
2021-04-13 15:15:22 +0200

    creation just one array (series,movies) and merging (by pushing)

--- js/script.js
+++ js/script.js
@@ -16,8 +16,7 @@ var app = new Vue (
         'pt-PT'
       ],
       userTitle:'',
-      movies:[],
-      series:[],
+      moviesAndSeries:[],
       myClass:"",
       myFlags:[
         'de',
@@ -45,53 +44,52 @@ var app = new Vue (
     },
     methods:{
       searchTitle: function(){
-        // static request to the server
-        // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
-        // .then((response) => {
-        //   this.movies = response.data.results;
-        // });
+        if (this.userTitle != '') {
+          // STATIC request to the server
+          // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
+          // .then((response) => {
+          //   this.movies = response.data.results;
+          // });
 
-        // dynamic request to the server
-        //for movies
-        axios.get( this.baseUrl + 'movie',{
-          params:{
-            api_key:this.apiKey,
-            query:this.userTitle,
-            language:this.languages[this.selectedLanguageIndex]
-          }
-        })
-        .then((response) => {
-          // console.log(response);
-          this.movies = response.data.results;
-        });
-        //for tv series
-        axios.get( this.baseUrl + 'tv',{
-          params:{
-            api_key:this.apiKey,
-            query:this.userTitle,
-            language:this.languages[this.selectedLanguageIndex]
-          }
-        })
-        .then((response) => {
-          this.series = response.data.results;
-          //cycle every obj in series array to push them in one array (movies)
-          this.series.forEach((item) => {
-            // console.log(item);
-            this.movies.push(item);
+          // DYNAMIC requests to the server
+          //cleaning the array moviesAndSeries at every event (without page reload) because of push
+          this.moviesAndSeries = [];
+          //axios for MOVIES
+          axios.get( this.baseUrl + 'movie',{
+            params:{
+              api_key:this.apiKey,
+              query:this.userTitle,
+              language:this.languages[this.selectedLanguageIndex]
+            }
+          })
+          .then((response) => {
+            // console.log(response);
+            //push with spread operator
+            this.moviesAndSeries.push(...response.data.results);
           });
-          // console.log(this.movies);
-          //cycle every vote.average in movies array to apply Math.ceil function
-          this.movies.forEach((item) => {
-            // console.log(item.vote_average);
-            item.vote_average = Math.ceil(item.vote_average / 2);
+          //axios for TV SERIES
+          axios.get( this.baseUrl + 'tv',{
+            params:{
+              api_key:this.apiKey,
+              query:this.userTitle,
+              language:this.languages[this.selectedLanguageIndex]
+            }
+          })
+          .then((response) => {
+            this.moviesAndSeries.push(...response.data.results);
+            //cycle every vote.average in movies array to apply Math.ceil function
+            this.moviesAndSeries.forEach((item) => {
+              // console.log(item.vote_average);
+              item.vote_average = Math.ceil(item.vote_average / 2);
+            });
           });
-        });
-        //default_image when item.poster_path == null
-        if ('https://image.tmdb.org/t/p/w300null') {
-          this.myClass = 'default_image';
+          //default_image when item.poster_path == null
+          if ('https://image.tmdb.org/t/p/w300null') {
+            this.myClass = 'default_image';
+          }
+          //cleaning
+          this.userTitle = '';
         }
-        //cleaning
-        this.userTitle = '';
       }
     }
   }
