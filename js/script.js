cfe550b07fe2000967c27e95bb124ed6529f49b5
Matteo <grecomatteo86@gmail.com>
2021-04-14 11:16:24 +0200

    moviesAxios refactoring

--- js/script.js
+++ js/script.js
@@ -46,28 +46,9 @@ var app = new Vue (
       searchTitle: function(){
         if (this.userTitle != '') {
           if (this.selected == 'All') {
-            // STATIC request to the server
-            // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
-            // .then((response) => {
-            //   this.movies = response.data.results;
-            // });
-
-            // DYNAMIC requests to the server
             //cleaning the array moviesAndSeries at every event (without page reload) because of push
             this.moviesAndSeries = [];
-            //axios for MOVIES
-            axios.get( this.baseUrl + 'movie',{
-              params:{
-                api_key:this.apiKey,
-                query:this.userTitle,
-                language:this.languages[this.selectedLanguageIndex]
-              }
-            })
-            .then((response) => {
-              // console.log(response);
-              //push with spread operator
-              this.moviesAndSeries.push(...response.data.results);
-            });
+            this.moviesAxios();
             //axios for TV SERIES
             axios.get( this.baseUrl + 'tv',{
               params:{
@@ -81,22 +62,11 @@ var app = new Vue (
               this.roundVote(this.moviesAndSeries);
               this.sortingVote(this.moviesAndSeries);
             });
-            //cleaning
+            //cleaning input
             this.userTitle = '';
           } else if (this.selected == 'Movies') {
             this.moviesAndSeries = [];
-            axios.get( this.baseUrl + 'movie',{
-              params:{
-                api_key:this.apiKey,
-                query:this.userTitle,
-                language:this.languages[this.selectedLanguageIndex]
-              }
-            })
-            .then((response) => {
-              this.moviesAndSeries.push(...response.data.results);
-              this.roundVote(this.moviesAndSeries);
-              this.sortingVote(this.moviesAndSeries);
-            });
+            this.moviesAxios();
             this.userTitle = '';
           } else if (this.selected == 'Series') {
             this.moviesAndSeries = [];
@@ -126,6 +96,28 @@ var app = new Vue (
       sortingVote:function (params) {
         //sorting movies and series by vote average (from high to low)
         params.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
+      },
+      moviesAxios:function () {
+        // STATIC request to the server
+        // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
+        // .then((response) => {
+        //   this.movies = response.data.results;
+        // });
+        // DYNAMIC requests to the server
+        axios.get( this.baseUrl + 'movie',{
+          params:{
+            api_key:this.apiKey,
+            query:this.userTitle,
+            language:this.languages[this.selectedLanguageIndex]
+          }
+        })
+        .then((response) => {
+          // console.log(response);
+          //push with spread operator
+          this.moviesAndSeries.push(...response.data.results);
+          this.roundVote(this.moviesAndSeries);
+          this.sortingVote(this.moviesAndSeries);
+        });
       }
     }
   }
