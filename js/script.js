deb02711fc627906e5e76ad15b65927962586e6c
Matteo <grecomatteo86@gmail.com>
2021-04-14 09:48:03 +0200

    add axios for movies and for series

--- js/script.js
+++ js/script.js
@@ -45,48 +45,85 @@ var app = new Vue (
     methods:{
       searchTitle: function(){
         if (this.userTitle != '') {
-          // STATIC request to the server
-          // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
-          // .then((response) => {
-          //   this.movies = response.data.results;
-          // });
 
-          // DYNAMIC requests to the server
-          //cleaning the array moviesAndSeries at every event (without page reload) because of push
-          this.moviesAndSeries = [];
-          //axios for MOVIES
-          axios.get( this.baseUrl + 'movie',{
-            params:{
-              api_key:this.apiKey,
-              query:this.userTitle,
-              language:this.languages[this.selectedLanguageIndex]
-            }
-          })
-          .then((response) => {
-            // console.log(response);
-            //push with spread operator
-            this.moviesAndSeries.push(...response.data.results);
-          });
-          //axios for TV SERIES
-          axios.get( this.baseUrl + 'tv',{
-            params:{
-              api_key:this.apiKey,
-              query:this.userTitle,
-              language:this.languages[this.selectedLanguageIndex]
-            }
-          })
-          .then((response) => {
-            this.moviesAndSeries.push(...response.data.results);
-            //cycle every vote.average in movies array to apply Math.ceil function
-            this.moviesAndSeries.forEach((item) => {
-              // console.log(item.vote_average);
-              item.vote_average = Math.ceil(item.vote_average / 2);
+          if (this.selected == 'All') {
+            // STATIC request to the server
+            // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
+            // .then((response) => {
+            //   this.movies = response.data.results;
+            // });
+
+            // DYNAMIC requests to the server
+            //cleaning the array moviesAndSeries at every event (without page reload) because of push
+            this.moviesAndSeries = [];
+            //axios for MOVIES
+            axios.get( this.baseUrl + 'movie',{
+              params:{
+                api_key:this.apiKey,
+                query:this.userTitle,
+                language:this.languages[this.selectedLanguageIndex]
+              }
+            })
+            .then((response) => {
+              // console.log(response);
+              //push with spread operator
+              this.moviesAndSeries.push(...response.data.results);
             });
-            //sorting movies and series by vote average (from high to low)
-            this.moviesAndSeries.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
-          });
-          //cleaning
-          this.userTitle = '';
+            //axios for TV SERIES
+            axios.get( this.baseUrl + 'tv',{
+              params:{
+                api_key:this.apiKey,
+                query:this.userTitle,
+                language:this.languages[this.selectedLanguageIndex]
+              }
+            })
+            .then((response) => {
+              this.moviesAndSeries.push(...response.data.results);
+              //cycle every vote.average in movies array to apply Math.ceil function
+              this.moviesAndSeries.forEach((item) => {
+                // console.log(item.vote_average);
+                item.vote_average = Math.ceil(item.vote_average / 2);
+              });
+              //sorting movies and series by vote average (from high to low)
+              this.moviesAndSeries.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
+            });
+            //cleaning
+            this.userTitle = '';
+          } else if (this.selected == 'Movies') {
+            this.moviesAndSeries = [];
+            axios.get( this.baseUrl + 'movie',{
+              params:{
+                api_key:this.apiKey,
+                query:this.userTitle,
+                language:this.languages[this.selectedLanguageIndex]
+              }
+            })
+            .then((response) => {
+              this.moviesAndSeries.push(...response.data.results);
+              this.moviesAndSeries.forEach((item) => {
+                item.vote_average = Math.ceil(item.vote_average / 2);
+              });
+              this.moviesAndSeries.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
+            });
+            this.userTitle = '';
+          } else if (this.selected == 'Series') {
+            this.moviesAndSeries = [];
+            axios.get( this.baseUrl + 'tv',{
+              params:{
+                api_key:this.apiKey,
+                query:this.userTitle,
+                language:this.languages[this.selectedLanguageIndex]
+              }
+            })
+            .then((response) => {
+              this.moviesAndSeries.push(...response.data.results);
+              this.moviesAndSeries.forEach((item) => {
+                item.vote_average = Math.ceil(item.vote_average / 2);
+              });
+              this.moviesAndSeries.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
+            });
+            this.userTitle = '';
+          }
         }
       }
     }
