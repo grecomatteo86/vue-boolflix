cfc9ce607f380925874ca780780c599b0b945c93
Matteo <grecomatteo86@gmail.com>
2021-08-03 23:08:42 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -47,11 +47,9 @@ var app = new Vue (
       searchTitle: function(){
         if (this.userTitle != '') {
           if (this.selected == '') {
-            //cleaning moviesAndSeries array at every event (without page reload) because of push
             this.moviesAndSeries = [];
             this.moviesAxios();
             this.seriesAxios();
-            //cleaning input
             this.userTitle = '';
           } else if (this.selected == 'Movies') {
             this.moviesAndSeries = [];
@@ -68,23 +66,14 @@ var app = new Vue (
         this.moviesAndSeries.splice(index,1);
       },
       roundVote:function (params) {
-        //cycle every vote.average in movies array to apply Math.ceil function
         params.forEach((item) => {
-          // console.log(item.vote_average);
           item.vote_average = Math.ceil(item.vote_average / 2);
         });
       },
       sortingVote:function (params) {
-        //sorting movies and series by vote average (from high to low)
         params.sort((a,b) => (a.vote_average > b.vote_average) ? -1 : 1);
       },
       moviesAxios:function () {
-        // STATIC request to the server
-        // axios.get('https://api.themoviedb.org/3/search/movie?api_key=bb946051787722f6361023f25c0639b5&query=ritorno+al+fut&language=it-IT')
-        // .then((response) => {
-        //   this.movies = response.data.results;
-        // });
-        // DYNAMIC requests to the server
         axios.get( this.baseUrl + 'movie',{
           params:{
             api_key:this.apiKey,
@@ -93,15 +82,12 @@ var app = new Vue (
           }
         })
         .then((response) => {
-          // console.log(response);
-          //push with spread operator
           this.moviesAndSeries.push(...response.data.results);
           this.roundVote(this.moviesAndSeries);
           this.sortingVote(this.moviesAndSeries);
         });
       },
       seriesAxios:function () {
-        //axios for TV SERIES
         axios.get( this.baseUrl + 'tv',{
           params:{
             api_key:this.apiKey,
